96acd56433dd2323128bd1a00ba46b385ef1c4a5
patrick-raedler <patoread@gmail.com>
2022-05-12 09:07:56 +0200

    feat: Create responsive modal
    The modal has been finished and it's now responsive on all types of screens

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,45 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { Icon } from "@iconify/vue";
+
+let isModalShown = ref(false);
+const toggleModal = () => {
+  isModalShown.value = !isModalShown.value;
+};
 </script>
 
 <template>
-  <nav class="text-black h-16 w-fullflex justify-center items-center p-4">
-    <div class="w-4/5 mx-auto flex justify-between items-center">
+  <nav class="text-black h-24 w-full flex justify-center items-center p-4">
+    <div class="w-3/4 md:w-3/5 mx-auto flex justify-between items-center">
       <a href="/" class="flex items-center space-x-1">
         <Icon class="w-12 h-12" icon="pepicons:book-print" color="black" />
-        <h1 class="hidden md:block">Quote</h1>
+        <h1 class="hidden md:block md:text-xl">Quotes</h1>
       </a>
 
-      <div class="relative md:hidden">
-        <button class="hover:ring-2 hover:rounded hover:ring-black">
+      <ul class="hidden md:flex md:text-base md:space-x-4">
+        <li class="w-fit hover:underline decoration-2">
+          <a href="/">Add a quote</a>
+        </li>
+        <li class="w-fit hover:underline decoration-2">
+          <a href="/">About</a>
+        </li>
+      </ul>
+
+      <div class="relative md:hidden" @click="toggleModal">
+        <button
+          class="hover:ring-2 hover:rounded hover:ring-slate-600 flex justify-center items-center"
+        >
           <Icon
             class="w-9 h-9"
             icon="heroicons-solid:menu-alt-2"
             color="black"
           />
         </button>
-        <div class="absolute -bottom-18 right-0 z-10">
-          <ul class="text-base ring-2 ring-black rounded p-2 space-y-2">
+        <div
+          v-if="isModalShown"
+          class="absolute -bottom-18 right-0 z-10 bg-gradient-to-tr from-white to-blue-200"
+        >
+          <ul class="text-base ring-2 ring-slate-600 rounded p-2 space-y-2">
             <li class="w-25 hover:underline decoration-2">
               <a href="/">Add a quote</a>
             </li>

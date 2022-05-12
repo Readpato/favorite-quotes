2cc2d124b5adca1c19f76f5bb9ebf9b5a3428483
patrick-raedler <patoread@gmail.com>
2022-05-12 11:16:12 +0200

    style: Changed main color to black
    The main color of the app has been changed to black

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import NavBar from "./components/NavBar.vue";
 // import { RouterLink, RouterView } from "vue-router";
 </script>
-
+// bg-gradient-to-tr from-white to-blue-200
 <template>
-  <div
-    class="font-poppins w-screen h-screen bg-gradient-to-tr from-white to-blue-200"
-  >
+  <div class="font-poppins w-screen h-screen bg-black">
     <NavBar />
   </div>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,10 +9,10 @@ const toggleModal = () => {
 </script>
 
 <template>
-  <nav class="text-black h-24 w-full flex justify-center items-center p-4">
+  <nav class="text-white h-24 w-full flex justify-center items-center p-4">
     <div class="w-3/4 md:w-3/5 mx-auto flex justify-between items-center">
       <a href="/" class="flex items-center space-x-1">
-        <Icon class="w-12 h-12" icon="pepicons:book-print" color="black" />
+        <Icon class="w-12 h-12" icon="pepicons:book" color="white" />
         <h1 class="hidden md:block md:text-xl">Quotes</h1>
       </a>
 
@@ -27,19 +27,19 @@ const toggleModal = () => {
 
       <div class="relative md:hidden" @click="toggleModal">
         <button
-          class="hover:ring-2 hover:rounded hover:ring-slate-600 flex justify-center items-center"
+          class="hover:ring-2 hover:rounded hover:ring-white flex justify-center items-center"
         >
           <Icon
             class="w-9 h-9"
             icon="heroicons-solid:menu-alt-2"
-            color="black"
+            color="white"
           />
         </button>
         <div
           v-if="isModalShown"
-          class="absolute -bottom-18 right-0 z-10 bg-gradient-to-tr from-white to-blue-200"
+          class="absolute -bottom-18 right-0 z-10 bg-black"
         >
-          <ul class="text-base ring-2 ring-slate-600 rounded p-2 space-y-2">
+          <ul class="text-base ring-2 ring-white rounded p-2 space-y-2">
             <li class="w-25 hover:underline decoration-2">
               <a href="/">Add a quote</a>
             </li>
